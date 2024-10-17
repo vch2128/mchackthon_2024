23336927a13e1ceef091d5698a9643c4cc48f1a9
vch2128 <bonniechuang920915@gmail.com>
2024-10-18 00:59:14 +0800

    add TechBoard page

---
+++ frontend/src/pages/Tech/TechBoard.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState, useContext } from 'react'
+import { getPosts, getSenderPosts, Post } from './types/post'
+import PostWithTabs from './components/PostWithTabs'
+import { UserContext } from '../../context/UserContext'
+
+const TechBoard: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([])
+  const [senderPosts, setSenderPosts] = useState<Post[]>([])
+  const { user } = useContext(UserContext);
+  
+  const refreshPosts = async () => {
+    const posts = await getPosts()
+    const sortedMessages = posts.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+    setPosts(sortedMessages)
+    const senderPosts = await getSenderPosts(user.id)
+    setSenderPosts(senderPosts)
+  }
+  useEffect(() => {
+    refreshPosts()
+  }, [])
+
+  return (
+    <div>
+      <div>
+        {/* <PostList posts={posts} /> */}
+        {<PostWithTabs allposts={posts} myposts={senderPosts} />}
+      </div>
+    </div>
+  )
+}
+
+export default TechBoard
