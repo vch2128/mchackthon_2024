3aae7745cb22fa608a841a69d0984b86dcedea8e
dennis <lintinwei20030906@gmail.com>
2024-10-17 20:55:48 +0800

    add api test shell script

--- backend/src/test.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST "http://localhost:3001/api/techpost" \
-     -H "Content-Type: application/json" \
-     -d '{
-           "content": "This is a new tech post",
-           "sender_id": "employee123"
-         }'
