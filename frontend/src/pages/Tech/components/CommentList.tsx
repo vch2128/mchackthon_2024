3688bc044daebeb51a861a833b4f49965c4e3e74
vch2128 <bonniechuang920915@gmail.com>
2024-10-19 20:41:11 +0800

    add commenting money bonus

--- frontend/src/pages/Tech/components/CommentList.tsx
+++ frontend/src/pages/Tech/components/CommentList.tsx
@@ -1,8 +1,8 @@
 import { List } from 'antd';
 import { Comment } from '@ant-design/compatible';
 import { CheckCircleOutlined} from '@ant-design/icons';
-import { Comment_t } from '../types/comment';
-import { updateBestComment } from '../types/post';
+import { Comment_t, updateWallet } from '../types/comment';
+import { updateBestComment} from '../types/post';
 import dayjs from 'dayjs';
 
 const dateStringFormat = (dateString: string) => dayjs(dateString).format('YYYY-MM-DD HH:mm:ss')
@@ -14,15 +14,14 @@ interface CommentListProps {
 }
 
 const CommentList: React.FC<CommentListProps> = ({ comments, refetchComments, isSender }) => {
-  console.log("CommentList props:", { comments: comments.length, refetchComments: !!refetchComments, isSender });
 
   const avatarUrl = "https://shoplineimg.com/643616b7087ae8002271ceb2/64e073d381afe80022a66ebc/1200x.webp?source_format=png"; // Define the avatar URL
 
-  console.log("is sender", isSender);
-  const handleSetBestComment = async (tech_post_id: string, commentId: string, setBest: boolean) => {
+  const handleSetBestComment = async (tech_post_id: string, commentId: string, setBest: boolean, comment_sender_id: string) => {
     try {
       await updateBestComment(tech_post_id, commentId, setBest);
-      refetchComments(); // Call the refetch function after updating
+      await updateWallet(comment_sender_id, 3);
+      refetchComments();
     } catch (error) {
       console.error("Error updating best comment:", error);
     }
@@ -50,7 +49,7 @@ const CommentList: React.FC<CommentListProps> = ({ comments, refetchComments, is
           actions={[
             isSender && (
               <span key="comment-set-best" 
-                    onClick={() => handleSetBestComment(item.tech_post_id, item.id, !item.is_best)} 
+                    onClick={() => handleSetBestComment(item.tech_post_id, item.id, !item.is_best, item.sender_id)} 
                     style={{ cursor: 'pointer' }}>
                 {item.is_best ? <CheckCircleOutlined /> : []}
                 {item.is_best ? ' Best' : ' Set as Best'}
