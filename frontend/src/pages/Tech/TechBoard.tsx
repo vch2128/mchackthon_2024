6af45a21818b04f6559192fba05e281b2e27d941
vch2128 <bonniechuang920915@gmail.com>
2024-10-19 12:00:56 +0800

    add search tech post

--- frontend/src/pages/Tech/TechBoard.tsx
+++ frontend/src/pages/Tech/TechBoard.tsx
@@ -1,28 +1,46 @@
 import { useEffect, useState, useContext } from 'react'
-import { getPosts, getSenderPosts, Post } from './types/post'
+import { getPosts, getSearchPostsMy, getSearchPosts, getSenderPosts, Post } from './types/post'
 import PostWithTabs from './components/PostWithTabs'
 import { UserContext } from '../../context/UserContext'
 
-const TechBoard: React.FC = () => {
+interface TechBoardProps {
+  isSearching: boolean;
+  searchQuery: string;
+}
+
+const TechBoard: React.FC<TechBoardProps> = ({ isSearching, searchQuery }) => {
   const [posts, setPosts] = useState<Post[]>([])
   const [senderPosts, setSenderPosts] = useState<Post[]>([])
   const { user } = useContext(UserContext);
   
   const refreshPosts = async () => {
     console.log('User ID:', user.id); // Print user ID to the terminal
-    try {
-      const posts = await getPosts()
-      const sortedPosts = posts.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-      setPosts(sortedPosts)
-      const senderPosts = await getSenderPosts(user.id)
-      setSenderPosts(senderPosts)
-    } catch (error) {
-      console.error('Error fetching posts:', error)
+    if (!isSearching){
+      try {
+        const posts = await getPosts()
+        const sortedPosts = posts.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+        setPosts(sortedPosts)
+        const senderPosts = await getSenderPosts(user.id)
+        setSenderPosts(senderPosts)
+      } catch (error) {
+        console.error('Error fetching posts:', error)
+      }
+    }else{
+      try{
+        const posts = await getSearchPosts(searchQuery)
+        setPosts(posts)
+        const senderPosts = await getSearchPostsMy(searchQuery, user.id)
+        setSenderPosts(senderPosts)
+      }catch(error){
+        console.error('Error fetching search posts:', error)
+        console.log("posts: ", posts)
+        console.log("senderPosts: ", senderPosts)
+      }
     }
   }
   useEffect(() => {
     refreshPosts()
-  }, [])
+  }, [isSearching, searchQuery])
 
   return (
     <div >
